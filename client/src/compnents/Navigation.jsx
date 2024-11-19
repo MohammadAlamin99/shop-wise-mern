e81981547b1ba07746d021b673d4348969d5135c
Mohammad Al Amin <alamin.cse.99@gmail.com>
2024-11-19 19:52:41 +0600

    change font-end design

---
+++ client/src/compnents/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../assets/images/navigation-img/logo_dark.png'
+import { CiHeart } from "react-icons/ci";
+import { PiShoppingCartLight } from "react-icons/pi";
+import { PiUserThin } from "react-icons/pi";
+const Navigation = () => {
+    return (
+        <div>
+            <section className='navigation'>
+               
+            </section>
+        </div>
+    );
+};
+
+export default Navigation;
