f277c9c94c2bdf80cfb3df8c7deec4987149739e
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-01-24 20:18:22 +0600

    user loging and registration problem fixed

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,11 +1,18 @@
 var jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const userModel = require("../models/userModel");
 
 exports.registration = async (req) => {
   try {
-    let reqBody = req.body;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    let reqBody = {
+      fullName: req.body.fullName,
+      userName: req.body.fullName,
+      email: req.body.email,
+      password: hashedPassword,
+    };
     let data = await userModel.create(reqBody);
-    return { status: "success", message: data };
+    return { status: "success", message: "User SignUp Successfull" };
   } catch (e) {
     return { status: "fail", message: e };
   }
@@ -14,23 +21,29 @@ exports.registration = async (req) => {
 // user log_in
 exports.login = async (req) => {
   try {
-    let reqBody = req.body;
+    let email = req.body.email;
+    let data = await userModel.aggregate([{ $match: { email: email } }]);
 
-    let data = await userModel.aggregate([
-      { $match: reqBody },
-      { $project: { password: 0 } },
-    ]);
     if (data.length > 0) {
-      let token = jwt.sign(
-        {
-          user_id: data[0]._id,
-          user_fullName: data[0].fullName,
-          email: data[0].email,
-        },
-        process.env.AUTH_KEY || "hello1234",
-        { expiresIn: "1h" }
+      const isValidPass = await bcrypt.compare(
+        req.body.password,
+        data[0].password
       );
-      return { status: "success", token: token, message: "Login Success" };
+
+      if (isValidPass == true) {
+        let token = jwt.sign(
+          {
+            user_id: data[0]._id,
+            user_fullName: data[0].fullName,
+            email: data[0].email,
+          },
+          process.env.AUTH_KEY || "hello1234",
+          { expiresIn: "1h" }
+        );
+        return { status: "success", token: token, message: "Login Success" };
+      } else {
+        return { status: "fail", message: "Password is wrong" };
+      }
     } else {
       return { status: "fail", message: "User Not found" };
     }
