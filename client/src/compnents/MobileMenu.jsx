cdce89763e7eb37fc7c47debacb11edcc1c05b27
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-05-01 22:49:07 +0600

    phone accout responsive cheaked

--- client/src/compnents/MobileMenu.jsx
+++ client/src/compnents/MobileMenu.jsx
@@ -49,81 +49,47 @@ const MobileMenu = ({ isActive, isSetActive }) => {
             <div className="mobile_nav">
               <ul>
                 <li>
-                  <a href="#">Home</a>
+                  <a href="/">Home</a>
                 </li>
                 <li>
-                  <a href="#">Shop</a>
+                  <a href="/shop">Shop</a>
                 </li>
                 <li>
-                  <a href="#">Product</a>
+                  <a href="/blog">Blog</a>
                 </li>
                 <li>
-                  <a href="#">Contact Us</a>
+                  <a href="/contact">Contact Us</a>
                 </li>
               </ul>
             </div>
           </div>
 
           <div className="bottom">
-            <a href="#" style={{ display: "block" }}>
+            <a href="/account">
               <div className="mobile-cart-list">
-                <h4 className="common-text">Cart</h4>
+                <h4 className="common-text">Account</h4>
                 <div className="icon">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="22"
+                    height="22"
                     viewBox="0 0 24 24"
                     fill="none"
                   >
                     <path
-                      d="M9 6L9 7C9 8.65685 10.3431 10 12 10C13.6569 10 15 8.65685 15 7V6"
-                      stroke="#141718"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M15.6113 3H8.38836C6.433 3 4.76424 4.41365 4.44278 6.3424L2.77612 16.3424C2.36976 18.7805 4.24994 21 6.72169 21H17.278C19.7498 21 21.6299 18.7805 21.2236 16.3424L19.5569 6.3424C19.2355 4.41365 17.5667 3 15.6113 3Z"
+                      d="M18.5588 19.5488C17.5654 16.8918 15.0036 15 12 15C8.99638 15 6.4346 16.8918 5.44117 19.5488M18.5588 19.5488C20.6672 17.7154 22 15.0134 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 15.0134 3.33285 17.7154 5.44117 19.5488M18.5588 19.5488C16.8031 21.0756 14.5095 22 12 22C9.49052 22 7.19694 21.0756 5.44117 19.5488M15 9C15 10.6569 13.6569 12 12 12C10.3431 12 9 10.6569 9 9C9 7.34315 10.3431 6 12 6C13.6569 6 15 7.34315 15 9Z"
                       stroke="#141718"
                       stroke-width="1.5"
                       stroke-linejoin="round"
                     />
                   </svg>
-                  <p className="common-text">2</p>
                 </div>
               </div>
             </a>
 
-            <div className="mobile-cart-list">
-              <h4 className="common-text">Cart</h4>
-              <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M9 6L9 7C9 8.65685 10.3431 10 12 10C13.6569 10 15 8.65685 15 7V6"
-                    stroke="#141718"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M15.6113 3H8.38836C6.433 3 4.76424 4.41365 4.44278 6.3424L2.77612 16.3424C2.36976 18.7805 4.24994 21 6.72169 21H17.278C19.7498 21 21.6299 18.7805 21.2236 16.3424L19.5569 6.3424C19.2355 4.41365 17.5667 3 15.6113 3Z"
-                    stroke="#141718"
-                    stroke-width="1.5"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <p className="common-text">2</p>
-              </div>
-            </div>
-
-            <button className="button">Sign In</button>
+            <a href="/signin" className="button">
+              Sign In
+            </a>
 
             <div className="mobile-social-media">
               <a href="#">
