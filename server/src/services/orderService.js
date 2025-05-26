d55daa6a38c838a8c6c137b2f6ac42de0a15e92c
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-05-26 20:00:38 +0600

    use paginate for order data get

--- server/src/services/orderService.js
+++ server/src/services/orderService.js
@@ -1,9 +1,36 @@
 const InvoiceModel = require("../models/InvoiceModel");
 
+// exports.getOrderList = async (req, res) => {
+//   try {
+//     let data = await InvoiceModel.find();
+//     return res.status(200).json({ status: "success", message: data });
+//   } catch (e) {
+//     return res.status(500).json({ status: "error", message: e.message });
+//   }
+// };
+
 exports.getOrderList = async (req, res) => {
   try {
-    let data = await InvoiceModel.find();
-    return res.status(200).json({ status: "success", message: data });
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 3;
+    const skip = (page - 1) * limit;
+
+    const total = await InvoiceModel.countDocuments();
+    const data = await InvoiceModel.find().skip(skip).limit(limit);
+    const totalPages = Math.ceil(total / limit);
+
+    return res.status(200).json({
+      status: "success",
+      data,
+      pagination: {
+        total,
+        totalPages,
+        currentPage: page,
+        limit,
+        hasNextPage: page < totalPages,
+        hasPreviousPage: page > 1,
+      },
+    });
   } catch (e) {
     return res.status(500).json({ status: "error", message: e.message });
   }
